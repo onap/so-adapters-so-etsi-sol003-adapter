6d17189199e05ff24a267da062c6bf2c6f61ebc8
Fiete Ostkamp <fiete.ostkamp@telekom.de>
2026-06-15 15:27:11 +0200

    Migrate to Java 17 and bump SO parent to 1.19.0
    - Update parent POM from 1.18.0-SNAPSHOT to 1.19.0
    - Set java.version and compiler target to 17
    - Update so-core-version to 1.19.0
    - Update Dockerfile base image to eclipse-temurin:17-jre-alpine
    - Add .mvn/jvm.config with --add-opens for Java 17 module system
    - Remove CryptoUtils usage (class removed in SO 1.19.0)
    - Update test configs with plaintext auth credentials
    
    Issue-ID: SO-4359
    Change-Id: I21c326969dcf48fc34ea60d8e90e9043db26ce3d
    Signed-off-by: Fiete Ostkamp <fiete.ostkamp@telekom.de>

--- so-etsi-sol003-adapter-common/src/main/java/org/onap/so/adapters/etsi/sol003/adapter/oauth/configuration/AuthorizationServerConfig.java
+++ so-etsi-sol003-adapter-common/src/main/java/org/onap/so/adapters/etsi/sol003/adapter/oauth/configuration/AuthorizationServerConfig.java
@@ -20,7 +20,6 @@
 
 package org.onap.so.adapters.etsi.sol003.adapter.oauth.configuration;
 
-import org.onap.so.utils.CryptoUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -37,15 +36,12 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     private static final int ONE_DAY = 60 * 60 * 24;
 
-    @Value("${vnfmadapter.auth:E39823AAB2739CC654C4E92B52C05BC34149342D0A46451B00CA508C8EDC62242CE4E9DA9445D3C01A3F13}")
+    @Value("${vnfmadapter.auth:vnfm:vnfm}")
     private String vnfmAdapterAuth;
-
-    @Value("${mso.key}")
-    private String msoEncryptionKey;
 
     @Override
     public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
-        final String[] decrypedAuth = CryptoUtils.decrypt(vnfmAdapterAuth, msoEncryptionKey).split(":");
+        final String[] decrypedAuth = vnfmAdapterAuth.split(":");
         final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         clients.inMemory().withClient(decrypedAuth[0]).secret(passwordEncoder.encode(decrypedAuth[1]))
                 .authorizedGrantTypes("client_credentials").scopes("write").accessTokenValiditySeconds(ONE_DAY)
